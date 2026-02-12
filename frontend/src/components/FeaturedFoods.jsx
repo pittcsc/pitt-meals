eb8b01e39286b5e67efc5e6a19087b52b9356cf1
Derin Uyar <138141560+Derin1000@users.noreply.github.com>
2026-02-12 18:18:25 -0500

    basic featured foods functionality

--- frontend/src/components/FeaturedFoods.jsx
+++ frontend/src/components/FeaturedFoods.jsx
@@ -8,9 +8,80 @@ const FeaturedFoods = () => {
     // For now, you will read in some mock data that will match the form of the actual data and use that to generate the list
     // Each group should only show the top three foods, but users should be able to expand the list to show more
 
-    return (
-        <div>{mockData}</div>
-    )
+  const [showMoreProtein, setShowMoreProtein] = useState(false)
+  const [showMoreCarbs, setShowMoreCarbs] = useState(false)
+  const [showMoreFiber, setShowMoreFiber] = useState(false)
+  const [showMoreCalories, setShowMoreCalories] = useState(false)
+
+  const sortedProtein = Object.entries(mockData).sort(([, a], [, b]) => b.protein - a.protein)
+  const sortedCarbs = Object.entries(mockData).sort(([, a], [, b]) => b.carbs - a.carbs)
+  const sortedFiber = Object.entries(mockData).sort(([, a], [, b]) => b.fiber - a.fiber)
+  const sortedCalories = Object.entries(mockData).sort(([, a], [, b]) => b.calories - a.calories)
+  
+  return (<>
+  <div>
+    <h1>High Protein</h1>   {/*Should I create another component for each list*/}
+    <ul>
+      {showMoreProtein ?
+        sortedProtein.map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.protein}</h2></li>)
+        ) :
+        sortedProtein.slice(0,3).map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.protein}</h2></li>)
+        )
+      }
+      <button id="show-more-protein" onClick={() => setShowMoreProtein(showMoreProtein => !showMoreProtein)}>{showMoreProtein ? "↑" : "↓"}</button>
+    </ul>
+
+    <h1>High Carb</h1>
+    <ul>
+      {showMoreCarbs ?
+        sortedCarbs.map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.carbs}</h2></li>)
+        ) :
+        sortedCarbs.slice(0,3).map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.carbs}</h2></li>)
+        )
+      }
+      <button id="show-more-carbs" onClick={() => setShowMoreCarbs(showMoreCarbs => !showMoreCarbs)}>{showMoreCarbs ? "↑" : "↓"}</button>
+    </ul>
+
+    <h1>High Fiber</h1>
+    <ul>
+      {showMoreFiber ?
+        sortedFiber.map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.fiber}</h2></li>)
+        ) :
+        sortedFiber.slice(0,3).map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.fiber}</h2></li>)
+        )
+      }
+      <button id="show-more-fiber" onClick={() => setShowMoreFiber(showMoreFiber => !showMoreFiber)}>{showMoreFiber ? "↑" : "↓"}</button>
+    </ul>
+
+    <h1>High Calorie</h1>
+    <ul>
+      {showMoreCalories ?
+        sortedCalories.map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.calories}</h2></li>)
+        ) :
+        sortedCalories.slice(0,3).map(([food, info]) => 
+        (
+          <li><h2>{food}: {info.calories}</h2></li>)
+        )
+      }
+      <button id="show-more-calories" onClick={() => setShowMoreCalories(showMoreCalories => !showMoreCalories)}>{showMoreCalories ? "↑" : "↓"}</button>
+    </ul>
+  </div>
+</>)
 }
+
 
 export default FeaturedFoods
