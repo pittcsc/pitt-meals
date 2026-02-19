83301d7e34b5536e73b8a0da4643b40a69ed9af1
Derin Uyar <138141560+Derin1000@users.noreply.github.com>
2026-02-19 13:10:41 -0500

    basic Featured Foods functionality (cleaned up code)

--- frontend/src/components/FeaturedFoods.jsx
+++ frontend/src/components/FeaturedFoods.jsx
@@ -10,16 +10,6 @@ const FeaturedFoods = () => {
     // For now, you will read in some mock data that will match the form of the actual data and use that to generate the list
     // Each group should only show the top three foods, but users should be able to expand the list to show more
 
-    
-  const [showMoreProtein, setShowMoreProtein] = useState(false)
-  const [showMoreCarbs, setShowMoreCarbs] = useState(false)
-  const [showMoreFiber, setShowMoreFiber] = useState(false)
-  const [showMoreCalories, setShowMoreCalories] = useState(false)
-
-  const sortedProtein = Object.entries(mockData).sort(([, a], [, b]) => b.protein - a.protein)
-  const sortedCarbs = Object.entries(mockData).sort(([, a], [, b]) => b.carbs - a.carbs)
-  const sortedFiber = Object.entries(mockData).sort(([, a], [, b]) => b.fiber - a.fiber)
-  const sortedCalories = Object.entries(mockData).sort(([, a], [, b]) => b.calories - a.calories)
   
   return (<>
   <div>
