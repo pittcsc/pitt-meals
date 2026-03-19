70d4445c908da4c6b46b51f799534ef02ae9228b
Derin Uyar <138141560+Derin1000@users.noreply.github.com>
2026-03-19 17:50:46 -0400

    ListFoods now parses breakfast.json

--- frontend/src/components/FeaturedFoods.jsx
+++ frontend/src/components/FeaturedFoods.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import mockData from "../../mockData/featuredFoods.json"
 import { useState } from "react";
+import { motion } from "motion/react"
+import breakfast from "../../../mock-data/breakfast.json"
+//import * as motion from "motion/react-client"
 
 
 const FeaturedFoods = () => {
@@ -12,37 +15,52 @@ const FeaturedFoods = () => {
 
   
   return (<>
-  <div style={{display:'flex', flexDirection:'column'}}>   
-    <ListFoods title="High Protein" data = {mockData} feature={'protein'} unit={'g'}></ListFoods>
-    <ListFoods title="High Carb" data = {mockData} feature={'carbs'} unit={'g'}></ListFoods>
-    <ListFoods title="High Fiber" data = {mockData} feature={'fiber'} unit={'g'}></ListFoods>
-    <ListFoods title="High Calorie" data = {mockData} feature={'calories'} unit={'kcal'}></ListFoods>
+  <div style={{display:'flex', flexDirection:'column'}}>  
+    <ListFoods title="High Protein" data = {breakfast.period.categories} feature={1}></ListFoods>
+    <ListFoods title="High Carb" data = {breakfast.period.categories} feature={2}></ListFoods>
+    <ListFoods title="High Fiber" data = {breakfast.period.categories} feature={7}></ListFoods>
+    <ListFoods title="High Calorie" data = {breakfast.period.categories} feature={0}></ListFoods>
     </div>
 </>)
 
 
 }
 
-const ListFoods = ({title, data, feature, unit}) => {
+const ListFoods = ({title, data, feature}) => {
   const [showMore, setShowMore] = useState(false)
-  const sortedList = Object.entries(data).sort(([, a], [, b]) => b?.[feature] - a?.[feature])
+  let sortedList = []
+  for(let i = 0; i < 5; i++){
+    sortedList = sortedList.concat(Object.entries(data[i].items))
+  }
+  sortedList.sort(([, a], [, b]) => b?.nutrients[[feature]].valueNumeric - a?.nutrients[[feature]].valueNumeric)
+
     return(
-    <div style={{padding:30, borderRadius:50, backgroundColor:'#090D11', margin:50, minWidth:500}}>
-      <h1 style={{paddingBottom:20}}>{title}</h1>
-      <ul>
-        {showMore ?
-          sortedList.map(([food, info]) => 
-          (
-            <li><h2>{food}: {info?.[feature]} {unit}</h2></li>)
-          ) :
-          sortedList.slice(0,3).map(([food, info]) => 
-          (
-            <li><h2>{food}: {info?.[feature]} {unit}</h2></li>)
-          )
-        }
-        <button style={{marginTop:20, fontWeight:'bold'}} id="show-more" onClick={() => setShowMore(showMore => !showMore)}>{showMore ? "↑" : "↓"}</button>
-      </ul>
-    </div>
+
+      /*I attempted to add animations via the motion library
+      but couldn't get it to work:
+      
+      <motion.div whileHover={{scale:1.2}} style={{padding:30, borderRadius:50, backgroundColor:'#090D11', margin:50, minWidth:500}}>
+      </motion.div>
+
+      */
+      
+
+      <div class="shadow-lg border-5 border-transparent hover:border-solid hover:border-[#636ebb] hover:scale-125" style={{padding:30, borderRadius:50, backgroundColor:'#090D11', margin:50, minWidth:500}}>
+        <h1 style={{paddingBottom:20}}>{title}</h1>
+        <ul>
+          {showMore ?
+            sortedList.map(([food, info]) => 
+            (
+              <li style={{marginTop: 15}}><h2>{info.name}: {info.nutrients[[feature]].valueNumeric} {info.nutrients[[feature]].uom}</h2></li>)
+            ) :
+            sortedList.slice(0,3).map(([food, info]) => 
+            (
+              <li style={{marginTop: 15}}><h2>{info.name}: {info.nutrients[[feature]].valueNumeric} {info.nutrients[[feature]].uom}</h2></li>)
+            )
+          }
+          <button style={{marginTop:20, fontWeight:'bold'}} id="show-more" onClick={() => setShowMore(showMore => !showMore)}>{showMore ? "↑" : "↓"}</button>
+        </ul>
+      </div>
     )
 }
 
